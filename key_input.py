9a2e11271c5d71723b3c5f4ce0b8a6dd285045af
charckle <andrej.zubin@email.com>
2019-12-05 21:10:43 +0100

    types of key-input added

--- key_input.py
+++ key_input.py
@@ -8,7 +8,7 @@ class KeyBoardHandler():
         self.keyboard = Controller()
         
     def parse_word(self, word):
-        keys = []
+        keys = []   
         for k in word:
             keys.append(k)
         return keys
@@ -16,6 +16,40 @@ class KeyBoardHandler():
     def push_keys(self, to_type):
         
         for k in self.parse_word(to_type):
-            self.keyboard.press(k)
-            self.keyboard.release(k)      
-
+            pres_release(k)
+    
+    def pres_release(self, key):
+        self.keyboard.press(key)
+        self.keyboard.release(key)
+    
+    def tab(self):
+        self.keyboard.press(Key.tab)
+        self.keyboard.release(Key.tab) 
+        
+    def alt_tab(self):
+        with self.keyboard.pressed(Key.alt_l):
+            self.keyboard.press(Key.tab)
+            self.keyboard.release(Key.tab)        
+    
+    def copy_paste(self, tip):
+        if tip == "v":
+            with self.keyboard.pressed(Key.ctrl_l):
+                self.keyboard.press('v')
+                self.keyboard.release('v')
+                
+        else:
+            with self.keyboard.pressed(Key.ctrl_l):
+                self.keyboard.press('c')
+                self.keyboard.release('c')
+    def enter(self):
+        self.keyboard.press(Key.enter)
+        self.keyboard.release(Key.enter)
+    
+    def escape(self):
+        self.keyboard.press(Key.esc)
+        self.keyboard.release(Key.esc)
+    
+    def shift_F_deset(self):
+        with self.keyboard.pressed(Key.shift_l):
+            self.keyboard.press(Key.f10)
+            self.keyboard.release(Key.f10)        
